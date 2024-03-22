d3cbab96516ffb14bc60d41eef69b6d6a9b5ea7b
Heman Sharma <hemansharma2002@gmail.com>
2024-03-22 15:13:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className=" bg-slate-950 text-white text-2xl font-bold h-screen w-full flex justify-center items-center">
+        This is just a test page for vercel deployment
+      </div>
+    </>
+  )
+}
+
+export default App
